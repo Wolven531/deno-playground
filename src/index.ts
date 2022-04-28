cdb36a851c0b6b183e223f9d9f4e8f600f8076bc
wolven531 <anthony.williams.cs@gmail.com>
2022-04-28 00:20:48 -0600

    Use handlers file

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
-import { serve } from "https://deno.land/std@0.133.0/http/server.ts";
-
-console.log("Hello, Deno!");
-
-const handler = (request: Request): Response => {
-  console.log(`handling request event - ${request.method} ${request.url}`);
-
-  const body = "Your user-agent is:\n\n".concat(
-    request.headers.get("user-agent") ?? "Unknown"
-  );
-
-  return new Response(body, { status: 200 });
-};
+import { serve } from "https://deno.land/std@0.136.0/http/mod.ts";
+import { httpRequestHandler } from "./handlers.ts";
 
 const env = Deno.env.toObject();
 
 // const PORT = parseInt(Deno.env.get("PORT") ?? "8080");
 const PORT = parseInt(env.PORT ?? "8080");
 
-console.log(`Starting HTTP webserver; access it at: http://localhost:${PORT}`);
+console.log(`Starting HTTP webserver; access it at http://localhost:${PORT}`);
 
-await serve(handler, { port: PORT });
+await serve(httpRequestHandler, { port: PORT });
