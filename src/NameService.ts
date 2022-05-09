e02df31cb2d893a7288f18a5554ab9444d5e7d82
wolven531 <anthony.williams.cs@gmail.com>
2022-05-08 19:21:09 -0600

    Create factory for service

--- src/NameService.ts
+++ src/NameService.ts
@@ -1,5 +1,32 @@
 export interface INameService {
+	/**
+	 * Get the current name value
+	 */
 	getName: () => string;
-	name: string;
+	// name: string;
+	/**
+	 * Set the current name value
+	 *
+	 * @param {string} newName New value for the name
+	 */
 	setName: (newName: string) => void;
 }
+
+/**
+ * This function returns an instance of INameService
+ *
+ * @param {string | undefined} startingName Optional value w/ which to start the instance
+ */
+export const NameServiceFactory: (startingName?: string) => INameService = (startingName) => {
+	let currentName = startingName ?? "";
+
+	return {
+		getName: () => currentName,
+		// name: currentName,
+		setName: (newName: string) => {
+			currentName = newName;
+		},
+	};
+};
+
+export default NameServiceFactory;
