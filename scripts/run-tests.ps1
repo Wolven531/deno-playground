6a6560a2c90ed8e44dde763b9710dd26d80ffad7
wolven531 <anthony.williams.cs@gmail.com>
2022-05-09 18:18:27 -0600

    Display code coverage

--- scripts/run-tests.ps1
+++ scripts/run-tests.ps1
@@ -5,3 +5,5 @@ DEL -Force -Recurse ./coverage
 
 # below line runs tests in parallel (default to num of CPUs available) w/ coverage
 deno test --allow-env --config ./deno.jsonc --jobs --coverage=./coverage ./test
+
+deno coverage ./coverage
