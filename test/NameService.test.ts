de741712ae741477fc5a8db23d5d969d8250996c
wolven531 <anthony.williams.cs@gmail.com>
2022-05-09 20:13:47 -0600

    Avoid dynamic import, dont run fmt

--- test/NameService.test.ts
+++ test/NameService.test.ts
@@ -1,14 +1,15 @@
 import { assertExists, fail } from "https://deno.land/std@0.138.0/testing/asserts.ts";
 // import * as assertions from "https://deno.land/std/testing/asserts.ts";
-import { INameService, NameServiceFactory } from "../src/NameService.ts";
+// import { INameService, NameServiceFactory } from "../src/NameService.ts";
+import * as NameService from "../src/NameService.ts";
 
 Deno.test({
 	name: "execute w/ default ctor",
 	fn(): void {
-		let svc: INameService;
+		let svc: NameService.INameService;
 
 		try {
-			svc = NameServiceFactory();
+			svc = NameService.NameServiceFactory();
 		} catch (err: any) {
 			fail("ctor should not throw error");
 		}
