ef2073e194a1e35c752505f840a57407ee833853
wolven531 <anthony.williams.cs@gmail.com>
2022-06-06 21:17:15 -0600

    Test w/o MongoClient

--- src/MongoService.ts
+++ src/MongoService.ts
@@ -1,27 +1,28 @@
-import { MongoClient } from 'https://deno.land/x/mongo@v0.30.0/src/client.ts';
+// import { MongoClient } from 'https://deno.land/x/mongo@v0.30.0/src/client.ts';
 
 export class MongoService {
-	private client: MongoClient;
+	// private client: MongoClient;
 	private connectionString: string;
 
 	constructor(connectionAddress: string) {
-		this.client = new MongoClient();
+		// this.client = new MongoClient();
 		this.connectionString = connectionAddress;
 	}
 
 	async fetchPages() {
 		try {
-			const db = await this.client.connect(this.connectionString);
+			return [];
+			// const db = await this.client.connect(this.connectionString);
 
-			const pagesCollection = db.collection('pages');
+			// const pagesCollection = db.collection('pages');
 
-			const results = pagesCollection.find({});
+			// const results = pagesCollection.find({});
 
-			const pages = await results.toArray();
+			// const pages = await results.toArray();
 
-			this.client.close();
+			// this.client.close();
 
-			return pages;
+			// return pages;
 		} catch (err) {
 			console.warn(
 				`Error connecting to DB`,
