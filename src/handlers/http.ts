9ddd7491e1a83ab46e2e0a85b86eb54804737d3d
wolven531 <anthony.williams.cs@gmail.com>
2022-08-02 21:55:46 -0600

    Start meta data for favicon alts

--- src/handlers/http.ts
+++ src/handlers/http.ts
@@ -68,6 +68,14 @@ const makeHtmlPage = (title: string, content: string): string => {
 <html>
 	<head>
 		<title>Anthony&apos;s Deno World - ${title}</title>
+		<!--
+		<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		-->
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="theme-color" content="#ffffff">
 	</head>
 	<body>
 ${content}
